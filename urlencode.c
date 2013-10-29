24e31fe41f319f185c3b582b25a6a0a81fa0c9db
Will Charczuk <will@clotheshor.se>
2013-10-29 19:43:24 -0400

    punting on unicode until i figure out how to referene a separate library for that.

--- urlencode.c
+++ urlencode.c
@@ -7,41 +7,44 @@
 // urlencode stuff
 // echo stuff | urlencode 
 
+//note unicode support should happen upstream of this.
 bool is_non_symbol(char c)
 {
-    if(c == '\0') return 1;
-    //if(c == '\r') return 1;
-    //if(c == '\n') return 1;
-    
+    if(c == '\0') return 1; //we want to write null regardless
     int c_int = (int)c;
     return (c_int >= 48 && c_int <= 57) || (c_int >= 65 && c_int <= 90) || (c_int >= 97 && c_int <= 122);
 }
 
 char *url_encode(const char *input)
 {
-    int i = 0;
-    int index_in_output = 0;
     int end = strlen(input);
-    size_t final_size = (end * 2) + 1; //+1 for terminating null
+    size_t final_size = (end * 3) + 1;
+    char *working = malloc(final_size * sizeof(char)), *output = working;
 
-    char *output = malloc(final_size * sizeof(char));
-
-    char c;
-    for(;i < end; i++)
+    while(*input)
     {
-        c = input[i];
-        if(is_non_symbol(c)) 
+        const char c = *input;
+        if(c < 0)
         {
-            sprintf(output+index_in_output, "%c", c);
-            index_in_output++;
+            input++;
+        }
+        else if(is_non_symbol(c)) 
+        {
+            *working++ = *input++;
         }
         else
         {
-            sprintf(output+index_in_output, "%%%02X", c);
-            index_in_output+=3;
+            char encoded[4] = {0};
+            snprintf(encoded, 4, "%%%02x", c);
+
+            *working++ = encoded[0];
+            *working++ = encoded[1];
+            *working++ = encoded[2];
+            input++;
         }
     }
-    output[index_in_output] = 0; //null terminate
+
+    *working = 0; //null term
     return output;
 }
 
